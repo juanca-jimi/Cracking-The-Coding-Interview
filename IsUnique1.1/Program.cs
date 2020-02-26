c52c1140d21b424a7dfe06c84f5c6dee7df9a847
Juan Carlos Jimenez <jcj237@outlook.com>
2020-02-26 08:47:29 -0500

    Began IsUnique - First problem First chapter. Came up with a brute force solution. Will begin testing once all listed solutions are complete.

--- IsUnique1.1/Program.cs
+++ IsUnique1.1/Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
 
+// Interview Question
+
+// Implement an algorithm to determine if a string has all unique characters. 
+// What if you cannot use additional data structures?
+
+
+
 namespace IsUnique1._1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //non-unique string
+            string isUnique = "sfdasyhidsjahfiuhushufhuehuehuheuheuhuwheuhruehwruhebfdhbah";
+
+            //Unique string
+            string isUnique1 = "qazwsxedcrfvtgb";
+
+            //First solution that comes to mind-----------------------------------------------------------------------------------
+
+            /*
+            for (int firstIndex = 0; firstIndex < length; firstIndex++)
+			{
+                string check = isUnique[firstIndex];
+                for (int secondIndex = 0; secondIndex < length; secondIndex++)
+			    {
+                    if (check == isUnique[second])
+	                {
+                        return "We have repeating characters";
+	                }
+			    }
+			}
+            return "We have no repeating characters!";
+            */
+            
+
+            //Solution from reading chapter---------------------------------------------------------------------------------------
+            //Solution shown in book----------------------------------------------------------------------------------------------
+
+
         }
     }
 }
